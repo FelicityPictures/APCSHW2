f7f352f7e06e5ae28bcadd136ce403045f712885
FelicityPictures <felicitypictures@yahoo.com>
2015-02-06 22:47:09 -0500

    Worked on Fib and Sqrt

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -5,15 +5,46 @@ public class Recursion{
   }
 
   public static int fact(int n){
-    if(n==1){
-      return 1;
+    if (n < 0) {
+      throw new IllegalArgumentException();
     }else{
-      return n * fact(n-1);
+      if(n==1){
+        return 1;
+      }else{
+        return n * fact(n-1);
+      }
     }
   }
 
-  public static void main(String[]args){
-    System.out.println(name());
-    System.out.println(fact(5));
+  //fibonacci: some numbers, starting with 47, start to become negative
+  public static int fib(int n){
+    if (n < 0) {
+      throw new IllegalArgumentException();
+    }else{
+      return f2(n,0,1);
+    }
   }
-}
+  public static  int f2(int n,int x,int y){
+    if(n == 0){
+      return x;
+    }else{
+      return f2(n-1,y,x+y);
+    }
+  }
+
+  public static double sqrt(double n){
+    if (n < 0) {
+      throw new IllegalArgumentException();
+    }else{
+      s2(n,1);
+    }
+    public static double s2(double n, double g){
+      if(//left off here
+    }
+
+      public static void main(String[]args){
+        System.out.println(name());
+        System.out.println(fact(5));
+        System.out.println(fact(-5));
+      }
+    }
