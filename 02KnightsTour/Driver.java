0a32a2cfcd438c8492c0b4bb31dd5aa2e6148e5c
FelicityPictures <felicitypictures@yahoo.com>
2015-02-12 12:28:22 -0500

    meow

--- 02KnightsTour/Driver.java
+++ 02KnightsTour/Driver.java
@@ -1,14 +1,9 @@
 public class Driver{
 
     public static void main(String[]args){
-	KnightsTour k;
-	if(args.length<3){
-	    System.out.println("Error");
-	}else{
-	    k = new KnightTour(args);
-	}
+	KnightsTour k = new KnightsTour(5);
 	k.solve();
-	System.out.println(k);
+	System.out.println(k.toString());
     }
 
 }
