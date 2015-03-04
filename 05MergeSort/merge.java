3cbe2a9fada06e7e18a054e739f820bfeb30f31b
FelicityPictures <felicitypictures@yahoo.com>
2015-03-04 01:00:56 -0500

    Something is wrong with my combine

--- 05MergeSort/merge.java
+++ 05MergeSort/merge.java
@@ -1,96 +1,83 @@
+import java.util.*;
+import java.io.*;
+
 public class merge{
 
-  public static int[] mergeSort(int[]orig){
+  public static void mergesort(int[]r){
+    r = cat(r);
+  }
+  
+  public static int[] cat(int[]orig){
     if(orig.length==1){
-      return orig;
-    }
-    if(orig.length == 2){
-      if(orig[0]<orig[1]){
-        return orig;
-      }else{
-        int x=orig[0];
-        orig[0]=orig[1];
-        orig[1]=x;
-        return orig;
-      }
     }else{
       int[]a=new int[orig.length/2];
       int[]b=new int[orig.length-(orig.length/2)];
-      for(int i=0;i<a.length;i++){
-        a[i]=orig[i];
-      }
-      for(int i=0;i<b.length;i++){
-        b[i]=orig[a.length+i];
-      }
-      a=mergeSort(a);
-      b=mergeSort(b);
-      orig=merge(a,b);
-      return orig;
-    }
-  }
-
-  private static int[] merge(int[]a,int[]b){
-    int[]x = new int[a.length+b.length];
-    if(a.length == 1){
-      if(a[0]<b[0]){
-        x[0]=a[0];
-        x[1]=b[0];
-        x[2]=b[1];
-      }else{
-          x[0]=b[0];
-            if(a[0]<b[1]){
-              x[1]=a[0];
-              x[2]=b[1];
-            }else{
-              x[1]=b[1];
-              x[2]=a[0];
-            }
-      }
-    }else{
       int ac=0;
       int bc=0;
-      int i=0;
-      while(ac<a.length && bc<b.length && i<x.length){
-        if(a[ac]<b[bc]){
-          x[i]=a[ac];
+      for(int i=0;i<orig.length;i++){
+        if(i<a.length){        
+          a[ac]=orig[i];
           ac++;
-          i++;
-        }
-        if(a[ac]<b[bc]){
-          x[i]=b[bc];
+        }else{
+          b[bc]=orig[i];
           bc++;
-          i++;
         }
       }
-      if(ac>bc){
-        while(bc<b.length){
-          x[i]=b[bc];
-          bc++;
-          i++;
-        }
-      }else{
-        while(ac<a.length){
-          x[i]=a[ac];
-          ac++;
-          i++;
-        }
-      }
-    }        
-    return x;
+      mergesort(a);
+      mergesort(b);
+      orig = combine(a,b);
+    }
+    return orig;
   }
 
-  public static void main(String[]arg){
-    int[]b=new int [9];
-    b[0]= 2;
-    b[1]= 20;
-    b[2]= 6;
-    b[3]= 87;
-    b[4]= 23489;
-    b[5]= 4;
-    b[6]= 456;
-    b[7]= 2;
-    b[8]= 890;
-    mergeSort(b);
-    System.out.println(b);
+  public static int[] combine(int[]a,int[]b){
+    int[]out = new int[a.length + b.length];
+    int i=0;
+    int aa=0;
+    int bb=0;
+    while(i<out.length){
+      if(aa<a.length && bb<b.length){
+        if(a[aa]<b[bb]){
+          out[i]=a[aa];
+          aa++;
+        }else{
+          out[i]=b[bb];
+          bb++;
+        }
+      }else{
+        if(aa>=a.length){
+          while(bb<b.length){
+            out[i]=b[bb];
+            bb++;
+          }
+        }else{
+          while(aa<a.length){
+            out[i]=a[aa];
+            aa++;
+          }
+        }
+      }
+      i++;
+    }
+    return out;
+  }
+
+  public static void displayIt(int[] c) {
+    String out = "[ ";
+    for (int i = 0; i < c.length - 1; ++i) {
+      out += c[i] + ", ";
+    }
+    out += c[c.length - 1] + " ]";
+    System.out.println(out);
+  }
+
+ public static void main(String[]arg){
+    Random rand = new Random(1);
+    int[]t=new int[8];
+    for(int i=0;i<t.length;i++){
+      t[i]=rand.nextInt(25);
+    }
+    mergesort(t);
+    displayIt(t);
   }
 }
--- 05MergeSort/sorts.java
+++ 05MergeSort/sorts.java
@@ -2,113 +2,81 @@ import java.util.*;
 import java.io.*;
 
 public class sorts{
-
-  public static int[] mergesort(int[]orig){
+  public static void mergesort(int[]r){
+    r = cat(r);
+  }
+  
+  public static int[] cat(int[]orig){
     if(orig.length==1){
-      return orig;
-    }
-    if(orig.length == 2){
-      if(orig[0]<orig[1]){
-        return orig;
-      }else{
-        int x=orig[0];
-        orig[0]=orig[1];
-        orig[1]=x;
-        return orig;
-      }
     }else{
       int[]a=new int[orig.length/2];
       int[]b=new int[orig.length-(orig.length/2)];
-      for(int i=0;i<a.length;i++){
-        a[i]=orig[i];
-      }
-      for(int i=0;i<b.length;i++){
-        b[i]=orig[a.length+i];
-      }
-      a=mergesort(a);
-      b=mergesort(b);
-      orig=merge(a,b);
-      return orig;
-    }
-  }
-
-  public static int[] merge(int[]a,int[]b){
-    int[]x = new int[a.length+b.length];
-    if(a.length == 1){
-      if(a[0]<b[0]){
-        x[0]=a[0];
-        x[1]=b[0];
-        try{
-        x[2]=b[1];
-        }catch(Exception IndexOutOfBounds){}
-      }else{
-        x[0]=b[0];
-        if(a[0]<b[1]){
-          x[1]=a[0];
-          try{
-          x[2]=b[1];
-          }catch(Exception IndexOutOfBounds){}
-        }else{
-          x[1]=b[1];
-          try{
-          x[2]=a[0];
-          }catch(Exception IndexOutOfBounds){}
-        }
-      }
-    }else{
       int ac=0;
       int bc=0;
-      //    int i=0;
-      for(int i=0;i<x.length;i++){
-        try{
-          if(a[ac]<b[bc]){
-            x[i]=a[ac];
-            ac++;
-          }
-        }catch(Exception IndexOutOfBounds){
-          if(ac>=bc){
-            x[i]=b[bc];
-            bc++;
-          }else{
-            x[i]=a[ac];
-          }
-        }
-        try{
-          if(a[ac]>=b[bc]){
-            x[i]=b[bc];
-            bc++;
-          }
-        }catch(Exception IndexOutOfBounds){
-          x[i]=a[ac];
+      for(int i=0;i<orig.length;i++){
+        if(i<a.length){        
+          a[ac]=orig[i];
           ac++;
+        }else{
+          b[bc]=orig[i];
+          bc++;
         }
       }
-      /*
-      if(ac>bc){
-        while(bc<b.length && i<x.length){
-          x[i]=b[bc];
-          bc++;
-          i++;
-        }
-      }else{
-        while(ac<a.length && i<x.length){
-          x[i]=a[ac];
-          ac++;
-          i++;
-          }
-          }
-      */
-    }        
-    return x;
+      mergesort(a);
+      mergesort(b);
+      orig = combine(a,b);
+    }
+    return orig;
   }
 
-  public static void main(String[]arg){
-    Random rand = new Random();
-    int[]b=new int [9];
-    for(int i=0;i<b.length;i++){
-      b[i]=rand.nextInt(10);
+  public static int[] combine(int[]a,int[]b){
+    int[]out = new int[a.length + b.length];
+    int i=0;
+    int aa=0;
+    int bb=0;
+    while(i<out.length){
+      if(aa<a.length && bb<b.length){
+        if(a[aa]<b[bb]){
+          out[i]=a[aa];
+          aa++;
+        }else{
+          out[i]=b[bb];
+          bb++;
+        }
+      }else{
+        if(aa>=a.length){
+          while(bb<b.length){
+            out[i]=b[bb];
+            bb++;
+          }
+        }else{
+          while(aa<a.length){
+            out[i]=a[aa];
+            aa++;
+          }
+        }
+      }
+      i++;
     }
-    mergesort(b);
-    System.out.println(b);
+    return out;
+  }
+
+  public static void displayIt(int[] c) {
+    String out = "[ ";
+    for (int i = 0; i < c.length - 1; ++i) {
+      out += c[i] + ", ";
+    }
+    out += c[c.length - 1] + " ]";
+    System.out.println(out);
+  }
+
+ public static void main(String[]arg){
+    Random rand = new Random(1);
+    int[]t=new int[8];
+    for(int i=0;i<t.length;i++){
+      t[i]=rand.nextInt(25);
+    }
+    mergesort(t);
+    displayIt(t);
   }
 }
