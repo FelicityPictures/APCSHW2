275af8cf5dd9c4b5802327a0d5c46b8621618652
FelicityPictures <felicitypictures@yahoo.com>
2015-03-10 23:48:07 -0400

    Finished quickSort

--- 06QuickSelect/Sorts.java
+++ 06QuickSelect/Sorts.java
@@ -2,17 +2,11 @@ import java.util.*;
 
 public class Sorts{
 
-  public static void quickSort(int[]a){
-    /*int start = 0;
-      int end = a.length-1;
-      int pivot=partition
-    */
-  }
   public static int partition(int[]a,int start,int end){
     if(start<0 || end>a.length-1){
       System.out.println("Out of Bounds");
     }
-    displayIt(a);
+    //  displayIt(a);
     Random rand = new Random();
     int randomValue=start+rand.nextInt((end-start)+1);
     int v = a[randomValue];
@@ -20,23 +14,13 @@ public class Sorts{
     int placeholder;
     int ei=end;
     int si=start;
-    System.out.println(v);
-
-    if(a[start]>v){
-      placeholder = a[start];
-      whereIsPivot=start;
-      a[start]=v;
-      a[randomValue]=placeholder;
-      si++;
-    }else{
-      placeholder = a[end];
-      whereIsPivot=end;
-      a[end]=v;
-      a[randomValue]=placeholder;
-      ei--;
-    }
-    displayIt(a);
-
+    //    System.out.println(v);
+    placeholder = a[end];
+    whereIsPivot=end;
+    a[end]=v;
+    a[randomValue]=placeholder;
+    ei--;
+    //   displayIt(a);
     while(ei!=si){
       if(a[si]>v){
         placeholder = a[ei];
@@ -46,43 +30,35 @@ public class Sorts{
       }else{
         si++;
       }
-      displayIt(a);
+      //     displayIt(a);
     }
-    placeholder=a[ei];
-    a[ei]=v;
-    a[whereIsPivot]=placeholder;
-    displayIt(a);
+    if(a[ei]>v){
+      placeholder=a[ei];
+      a[ei]=v;
+      a[whereIsPivot]=placeholder;
+    }else{
+      ei++;
+      placeholder=a[ei];
+      a[ei]=v;
+      a[whereIsPivot]=placeholder;
+    }
+    //   displayIt(a);
     return ei;
-  } 
-  /*int[]b=new int [a.length];
-    for(int i=0;i<a.length;i++){
-    if(i<start || i>end){
-    b[i]=a[i];
-    }
-    }
-    Random rand = new Random();
-    int randomValue=start+rand.nextInt((end-start)+1);
-    int v = a[randomValue];
-    //  System.out.println(v);
-    int ei=end;
-    for(int i=start;i<=end;i++){
-    if(a[i]<v){
-    b[start]=a[i];
-    start++;
-    }
-    if(a[i]>v){
-    b[ei] = a[i];
-    ei--;
-    }
-    //    displayIt(b);
-    }
-    b[ei]=v;
-    for(int i=0;i<b.length;i++){
-    a[i]=b[i];
-    }
-    return ei;
-  */
+  }
 
+  public static void quickSort(int[]a) {
+    meow(a,0,a.length-1);
+  }
+
+  private static void meow(int[]a,int start,int end) {
+    if(start<end){
+      int pivot = partition(a,start,end);
+      meow(a,start,pivot-1);
+      meow(a,pivot+1,end);
+    }
+  }
+
+  /*
   public static void displayIt(int[] c) {
     String out = "[ ";
     for (int i = 0; i < c.length - 1; ++i) {
@@ -91,8 +67,8 @@ public class Sorts{
     out += c[c.length - 1] + " ]";
     System.out.println(out);
   }
-
-public static void main(String[]args){
+  
+  public static void main(String[]args){
     int[]test=new int[10];
     Random r = new Random();
     test[0]= 13;
@@ -105,6 +81,10 @@ public static void main(String[]args){
     test[7]= 12;
     test[8]= 1;
     test[9]= 10;
-    partition(test,0,9);
+    displayIt(test);
+    quickSort(test);
+    displayIt(test);
   }
+*/
+
 }
