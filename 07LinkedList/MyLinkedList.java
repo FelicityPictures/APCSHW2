1ee22298bd73101d6d1c6d7b78b2d64a010128f5
FelicityPictures <felicitypictures@yahoo.com>
2015-03-15 21:54:35 -0400

    MyLinkedList

--- 07LinkedList/MyLinkedList.java
+++ 07LinkedList/MyLinkedList.java
@@ -1,83 +1,122 @@
 public class MyLinkedList{
-    private LNode start,end;
-    private int size;
+  private LNode start,end;
+  private int size;
     
-    public MyLinkedList(){
-	start = new LNode();
-	end = new LNode();
-	start.setNext(end);
-	size=0;
-    }
+  public MyLinkedList(){
+    size=0;
+  }
 
-    public MyLinkedList(int s){
-	super();
-	LNode plus = new LNode(s);
-	start.setNext(plus);
-	plus.setNext(end);
-	size=1;
+  public boolean add(int n){
+    if(size==0){
+      start=new LNode(n);
+      end=start;
+    }else{
+    LNode plus = new LNode(n);
+    end.setNext(plus);
+    end = plus;
     }
+    size++;
+    return true;
+  }
 
-    public boolean add(int n){
-	LNode plus = new LNode(n);
-	end.setNext(plus);
-	end = plus;
-	size++;
-	return true;
+  public boolean add(int n, int index){
+    if(index<0 || index>size){
+      throw new ArrayIndexOutOfBoundsException();
+    }else{
+      if(index==size){
+        return add(n);
+      }else{
+        LNode plus = new LNode(n);
+        LNode now = start;
+        for(int i=0;i<index-1;i++){
+          now=now.getNext();
+        }
+        plus.setNext(now.getNext());
+        now.setNext(plus);
+      }
+      size++;
+      return true;
     }
-
-    public boolean add(int n, int index){
-	if(index<0 || index>size){
-	    return false;
-	}else{
-	LNode plus = new LNode(n);
-	LNode now = start.getNext();
-	for(int i=0;i<index-1;i++){
-	    now=now.getNext();
-	}
-	plus.setNext(now.getNext());
-	now.setNext(plus);
-	size++;
-	return true;
-	}
-    }
+  }
     
-
-    //Needs try/catch
-    public int get(int index){
-	LNode now = start;
-	for(int i=0;i<index;i++){
+  public int get(int index){
+    if(index<0 || index>size-1){
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    LNode now = start;
+    for(int i=0;i<index;i++){
 	    now = now.getNext();
-	}
-	return now.getData();
     }
-    //Needs try/catch
-    public void set(LNode n, int index){
-	LNode now = start;
-	for(int i=0;i<index;i++){
+    return now.getData();
+  }
+
+  public void set(int n, int index){
+    if(index<0 || index>size-1){
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    LNode now = start;
+    for(int i=0;i<index;i++){
 	    now = now.getNext();
-	}
-	now.setData(n.getData());
     }
+    now.setData(n);
+  }
 
-    public int size(){
-	return size;
+  public int size(){
+    return size;
+  }
+
+  public void remove(int index){
+    LNode now = start;
+    for(int i=0;i<index-1;i++){
+	    now = now.getNext();
     }
+    LNode yes = now;
+    for(int i=0;i<2;i++){
+      yes = yes.getNext();
+    }
+    now.setNext(yes);
+    size--;
+  }
+  
+  public int IndexOf(int value){
+    int i=0;
+    LNode now = start;
+    while(i<size && now.getData() != value){
+      now=now.getNext();
+      i++;
+    }
+    if(i==size){
+      return -1;
+    }else{
+      return i;
+    }
+  }
 
-    public String toString(){
-	String output = "[ ";
-	LNode now = start.getNext();
-	while(now.getNext()!=null){
+  public String toString(){
+    String output = "[ ";
+    LNode now = start;
+    while(now.getNext()!=null){
 	    output = output+now.getData() + " ";
 	    now=now.getNext();
-	}
-	output = output+now.getData() + " ";
-	return output + "]";
     }
+    output = output+now.getData() + " ";
+    return output + "]";
+  }
 
-    public static void main(String[]meow){
-	MyLinkedList test = new MyLinkedList(2);
+  public static void main(String[]meow){
+    MyLinkedList test = new MyLinkedList();
+    test.add(3);
+    test.add(5);
+    test.add(3);
+    test.add(65);
+    test.add(48);
+    test.add(9870);
+    test.add(98);
+    test.add(2);
 		System.out.println(test.toString());
-	//	test.set(d,1);
-	System.out.println(test.size());
-    }
+    test.remove(5);
+    // System.out.println(test.toString());
+    System.out.println(test.IndexOf(9870));
+
+  }
 }
