97af127a0e718f48f9de15bbd53aedd4104f4653
FelicityPictures <felicitypictures@yahoo.com>
2015-03-24 18:28:41 -0400

    added name() and tests

--- 09Stacks/MyQueue.java
+++ 09Stacks/MyQueue.java
@@ -1,21 +1,43 @@
 public class MyQueue<T>{
-    private MyLinkedList<T>queue;
+  private MyLinkedList<T>queue;
 
-    public MyQueue(){
-	queue = new MyLinkedList<T>();
-    }
+  public MyQueue(){
+    queue = new MyLinkedList<T>();
+  }
 
-    //add element to queue
-    public void enqueue(T n){
-	queue.add(n);
-    }
+  //add element to queue
+  public void enqueue(T n){
+    queue.add(n);
+  }
 
-    public T dequeue(){
-	return queue.remove();
-    }
+  public T dequeue(){
+    return queue.remove();
+  }
 
-    public String toString(){
-	return queue.toString();
-    }
+  public String toString(){
+    return queue.toString();
+  }
+
+  public String name(){
+    return "ng.felicity";
+  }
+
+  public static void main(String[]meow){
+    MyQueue<Integer> test = new MyQueue<Integer>();
+    test.enqueue(3);
+    System.out.println(test);
+    test.enqueue(23);
+    test.enqueue(54);
+    System.out.println(test);
+    test.dequeue();
+    System.out.println(test);
+    test.enqueue(74);
+    System.out.println(test);
+    test.enqueue(3);
+    test.enqueue(5);
+    test.dequeue();
+    test.enqueue(6);
+    System.out.println(test);
+  }
 
 }
