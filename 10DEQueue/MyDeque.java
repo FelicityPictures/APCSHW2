7d200366cddb9485b755fdba4ffc56b8a0cd2dd5
FelicityPictures <felicitypictures@yahoo.com>
2015-03-25 12:27:23 -0400

    toString and tests

--- 10DEQueue/MyDeque.java
+++ 10DEQueue/MyDeque.java
@@ -28,14 +28,33 @@ public class MyDeque{
 	data=replace;
 
     }
-    /*
+    
     public void addFirst(Object value){
 	data[head]=value;
 	if(head-1 != tail){
 	    head--;
 	}else{
 	    resize();
-	    //change head
 	}
-	}*/
+    }
+    
+    public String toString(){
+	int h = head;
+	String out = "[ ";
+	while(h<data.length){
+	    out = out + data[h];
+	    h++;
+	}
+	for(int i=0;i<tail;i++){
+	    out = out + data[i];
+	}
+	return out;
+    }
+
+    //  public static void printIt(String){
+
+    public static void main(String[]meow){
+	MyDeque t = new MyDeque(5);
+	t.addFirst(2);
+    }
 }
