9c7f0dda02db4ec4c8a51e47fd3dfd281c26afab
FelicityPictures <felicitypictures@yahoo.com>
2015-04-23 12:14:17 -0400

    TreeNode WORKS

--- 12Tree/TreeNode.java
+++ 12Tree/TreeNode.java
@@ -2,7 +2,6 @@ public class TreeNode<T>{
     private T data;
     private TreeNode<T>left;
     private TreeNode<T>right;
-    private boolean full;
 
     public TreeNode(T value){
 	data = value;
@@ -14,6 +13,22 @@ public class TreeNode<T>{
 
     public void setData(T value){
 	data = value;
+    }
+
+    public TreeNode<T> getLeft(){
+	return left;
+    }
+
+    public void setLeft(TreeNode<T> plus){
+	left = plus;
+    }
+
+    public TreeNode<T> getRight(){
+	return right;
+    }
+
+    public void setRight(TreeNode<T> plus){
+	right = plus;
     }
 
     public boolean full(){
@@ -34,5 +49,19 @@ public class TreeNode<T>{
 	    }
 	    return true;
 	}
+    }
+    
+    public String toString(){
+	return "" + data;
+    }
+
+    public static void main(String[]meow){
+	TreeNode<Integer> a = new TreeNode<Integer>(5);
+	TreeNode<Integer> b = new TreeNode<Integer>(6);
+	TreeNode<Integer> c = new TreeNode<Integer>(7);
+	a.setLeft(b);
+	a.setRight(c);
+	System.out.println(a.toString());
+	System.out.println(a.getLeft().getData());
     }
 }
