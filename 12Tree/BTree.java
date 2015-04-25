72f902c796b6a8ca41bc02a3a720316453da7f5d
FelicityPictures <felicitypictures@yahoo.com>
2015-04-25 18:53:08 -0400

    Fixed add function I hope

--- 12Tree/BTree.java
+++ 12Tree/BTree.java
@@ -1,58 +1,88 @@
 import java.io.*;
 import java.util.*;
+//Random seed set at 2
 
 public class BTree<T>{
-    public static final int PRE_ORDER = 0;
-    public static final int IN_ORDER = 1;
-    public static final int POST_ORDER = 2;
+  public static final int PRE_ORDER = 0;
+  public static final int IN_ORDER = 1;
+  public static final int POST_ORDER = 2;
 
-    private TreeNode<T> root;
+  private TreeNode<T> root;
 
-    public BTree(){
-	root = null;
-    }
+  public BTree(){
+    root = null;
+  }
 
-    public void add(T d){
-	if(root == null){
-	    root.setData(d);
-	}else{
-	    Random r = new Random();
-	    int place = r.nextInt(2);
+  public void add(T d){
+    if(root == null){
+	    root = new TreeNode<T>(d);
+    }else{
 	    TreeNode<T> plus = new TreeNode<T>(d);
-	    //random
+      add(root,plus);
+    }
+  }
+
+  private void add(TreeNode<T> curr, TreeNode<T> bn){
+    //random
 	    //0: add left
 	    //1: add right
 	    //Recursive: Keep going randomly until you're able to add
-	    if(place==0){
-		add(root.getLeft(),plus);
-	    }else{
-		add(root.getRight(),plus);
-	    }
-	}
+    Random r = new Random(2);
+    int place = r.nextInt(2);
+    if(place == 0){
+      if(curr.getLeft() == null){
+        curr.setLeft(bn);
+      }else{
+        add(curr.getLeft(),bn);
+      }
+    }else{
+      if(curr.getRight() == null){
+        curr.setRight(bn);
+      }else{
+        add(curr.getRight(),bn);
+      }
     }
+  }
 
-    private void add(TreeNode<T> curr, TreeNode<T> bn){
-	 Random r = new Random();
-	 int place = r.nextInt(2);
-	 if(place == 0){
-	     if(curr.getLeft() == null){
-		 curr.setLeft(bn);
-	     }else{
-		 add(curr.getLeft(),bn);
-	     }
-	 }else{
-	     if(curr.getRight() == null){
-		 curr.setRight(bn);
-	     }else{
-		 add(curr.getRight(),bn);
-	     }
-	 }
-    }
+  //add modes to traverse
 
-    public void preOrder(TreeNode<T> curr){
-	String s = "[ " + curr.getData();
-	if(!curr.full()){
-	    //recursive?    
-	}
+  public void traverse(int mode){
+    if ( mode == PRE_ORDER )
+	    preOrder( root );
+    /*  else if ( mode == IN_ORDER )
+	    inOrder( root );
+    else
+	    postOrder( root );
+    */
+  }
+
+  public void preOrder(TreeNode<T> curr){
+    System.out.println("[ " + preO(curr) + "]");
+  }
+  private String preO(TreeNode<T> tree){
+    String s = "" + tree.getData() + " ";
+    if(!tree.hasSomething()){
+      return s;
+    }else{
+      if(tree.getLeft()!=null && tree.getRight()!=null){
+        return s + preO(tree.getLeft()) + " " + preO(tree.getRight()) + " ";
+      }else{
+        if(tree.getLeft()!=null){
+          return s + preO(tree.getLeft()) + " ";
+        }else{
+        return s + preO(tree.getLeft()) + " ";
+        }
+      }
     }
+  }
+
+  public static void main(String[]meow){
+    BTree<Integer>t = new BTree<Integer>();
+    t.add(1);
+    t.add(2);
+    t.add(3);
+    t.add(4);
+    t.traverse(0);
+  }
+
 }
