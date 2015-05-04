24632c7a1a4b08946025cbcc86bd4238893b8eac
FelicityPictures <felicitypictures@yahoo.com>
2015-05-03 23:13:11 -0400

    Worked on remove & made InOrder()

--- 12Tree/BSTree.java
+++ 12Tree/BSTree.java
@@ -50,16 +50,32 @@ public class BSTree<T extends Comparable>{
     ====================*/
   @SuppressWarnings("unchecked")
     private BSTreeNode<T> remove(BSTreeNode<T>curr,T c){
-    if(curr.getData()==c && isLeaf(curr)){
-      curr = null;
-    }else{
-      if(curr.getData().compareTo(c)>0){
-        curr.setLeft(remove(curr.getLeft(),c));
-	    }else{
-        curr.setRight(remove(curr.getRight(),c));
-	    }
+    /* if(curr==null){
+      return curr;
     }
+    if(curr.getData().compareTo(c)>0){
+      curr.setLeft(remove(curr.getLeft(),c));
+    }else{
+      if(curr.getData().compareTo(c)<0){
+        curr.setRight(remove(curr.getRight(),c));
+      }
+    }
+    if(!isLeaf(curr)){
+      curr.setData(findSmallest(curr.getRight()).getData());
+      curr.setRight(remove(curr.getRight(),curr.getData()));
+    }
+    curr=(curr.getLeft()!=null);*/
     return curr;
+  }
+  
+  private BSTreeNode<T> findSmallest(BSTreeNode<T> s){
+    if(s==null){
+      return null;
+    }
+    if(s.getLeft()==null){
+      return s;
+    }
+    return findSmallest(s.getLeft());
   }
 
 
@@ -70,23 +86,32 @@ public class BSTree<T extends Comparable>{
     Wrapper for the recursive inOrder method
     ====================*/
   public void inOrder() {
-    inOrderHelper( root );
-    System.out.println();
+    String s = "[ ";
+    System.out.print("[ ");
+    inOrderHelper(root);
+    System.out.print("]");
   }
 
-  /*======== public void inOrderHelper() ==========
-    Inputs:   BSTreeNode<T> t  
-    Returns: 
-      
-    Performs an in-order traversal for the tree with 
-    root t.
-    ====================*/
-  public void inOrderHelper( BSTreeNode<T> t ) {
-    if (t == null) 
-	    return;
-    inOrderHelper( t.getLeft() );
-    System.out.print( t.getData() + " ");
-    inOrderHelper( t.getRight() );
+  public void inOrderHelper(BSTreeNode<T>t) {
+    String s = "" + t.getData() + " ";
+    if(isLeaf(t)){
+      System.out.print(s);
+    }else{
+      if(t.getLeft()!=null && t.getRight()!=null){
+        inOrderHelper(t.getLeft());
+        System.out.print(s);
+        inOrderHelper(t.getRight());
+      }
+      if(t.getLeft()!=null && t.getRight()==null){
+        inOrderHelper(t.getLeft());
+        System.out.print(s);
+      }
+      if(t.getLeft()==null && t.getRight()!=null){
+        System.out.print(s);
+        inOrderHelper(t.getRight());
+      }
+    }
+    System.out.print("");
   }
 
   /**
@@ -102,15 +127,12 @@ public class BSTree<T extends Comparable>{
 		if(r == null){
       return 0;
 		}else{
-      //System.out.println("recursion height");
       return 1 + Math.max(getHeight(r.getLeft()),
                           getHeight(r.getRight()));
 		}
   }
 
   private int maxLength() {
-		// returns the minimum number of characters required
-		// to print the data from any node in the tree
 		if (root == null)
       return 0;
 		return maxLength(root);
@@ -133,35 +155,11 @@ public class BSTree<T extends Comparable>{
   }
 
   private String spaces(double n) {
-		// returns a String of n spaces
 		String result = "";
 		for (int i = 0; i < n; i++)
       result += " ";
 		return result;
   }
-
-  /*
-    getLevel will produce a String for each level of the tree.
-    The resulting Strings will look like this:
-
-    ._______________________________
-    ._______________._______________
-    ._______._______._______._______
-    .___.___.___.___.___.___.___.___
-    ._._._._._._._._._._._._._._._._
-
-    toString will combine those Strings and provide an output that
-    will look like this:
-
-    _______________.
-    _______._______________.
-    ___._______._______._______.
-    _.___.___.___.___.___.___.___.
-    ._._._._._._._._._._._._._._._.
-    In these diagrams, each dot represents wordLength characters,
-    each underscore represents wordLength spaces, and, for any nodes
-    that are null, the dots will be "replaced" by underscores.
-  */
 
   private String getLevel(BSTreeNode<T> curr, int currLevel, int targetLevel, int height, int wordLength) {
 		if (currLevel == 1){
@@ -191,16 +189,11 @@ public class BSTree<T extends Comparable>{
 		String result = "";
 		int height = getHeight();
 		int wordLength = maxLength();
-		// add the every level of the tree except the last one
 		for (int level = 1; level < height; level++){
-			// remove extra spaces from the end of each level's String to prevent lines from
-			// getting unnecessarily long and add spaces to the front of each level's String
-			// to keep everything centered
 			result += spaces(wordLength * Math.pow(2, height - level) - wordLength) +
 				getLevel(root, level, level, height, wordLength).replaceFirst("\\s+$", "") +
 				"\n";
 		}
-		// now add the last level (level = height)
 		result += getLevel(root, height, height, height, wordLength).replaceFirst("\\s+$", "");
 				
 		return result;
@@ -216,8 +209,8 @@ public class BSTree<T extends Comparable>{
     t.add(50);
     t.add(0);
     System.out.println(t.toString());
-    t.remove(13);
-    System.out.println("\n \n \n"+t.toString());
+    // t.remove(13);
+    t.inOrder();
   }
 
 }
