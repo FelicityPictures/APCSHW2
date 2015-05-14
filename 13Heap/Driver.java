3734766ddc0bf8b2c24fd472957232f71d70c355
FelicityPictures <felicitypictures@yahoo.com>
2015-05-14 12:05:02 -0400

    improved remove

--- 13Heap/Driver.java
+++ 13Heap/Driver.java
@@ -1,40 +1,70 @@
 public class Driver{
     
-  public static void out(String s){
-    System.out.println(s);
-  }
+    public static void out(String s){
+	System.out.println(s);
+    }
 
-  public static void main(String[]meow){
-    MyHeap t = new MyHeap();
-    t.add(5);
-    t.add(4);
-    t.add(3);
-    t.add(7);
-    t.add(2);
-    t.add(1);
-    t.add(10);
-    t.add(6);
-    t.add(9);
-    t.add(8);
-    out("Before Removal of: " + t.size());
-    out(t.toString());
-    t.remove();
-    out("After");
-    out(t.toString() + "\n");
-    out("Before Removal of: " + t.size());
-    out(t.toString());
-    t.remove();
-    out("After");
-    out(t.toString() + "\n");
-    out("Before Removal of: " + t.size());
-    out(t.toString());
-    t.remove();
-    out("After");
-    out(t.toString() + "\n");
-    out("Before Removal of: " + t.size());
-    out(t.toString());
-    t.remove();
-    out("After");
-    out(t.toString() + "\n");
-  }
+    public static void main(String[]meow){
+	MyHeap t = new MyHeap();
+	t.add(5);
+	t.add(4);
+	t.add(3);
+	t.add(7);
+	t.add(2);
+	t.add(1);
+	t.add(10);
+	t.add(6);
+	t.add(9);
+	t.add(8);
+	out("Before Removal of: " + t.size());
+	out(t.toString());
+	t.remove();
+	out("After");
+	out(t.toString() + "\n");
+	out("Before Removal of: " + t.size());
+	out(t.toString());
+	t.remove();
+	out("After");
+	out(t.toString() + "\n");
+	out("Before Removal of: " + t.size());
+	out(t.toString());
+	t.remove();
+	out("After");
+	out(t.toString() + "\n");
+	out("Before Removal of: " + t.size());
+	out(t.toString());
+	t.remove();
+	out("After");
+	out(t.toString() + "\n");
+	out("Before Removal of: " + t.size());
+	out(t.toString());
+	t.remove();
+	out("After");
+	out(t.toString() + "\n");
+	out("Before Removal of: " + t.size());
+	out(t.toString());
+	t.remove();
+	out("After");
+	out(t.toString() + "\n");
+	out("Before Removal of: " + t.size());
+	out(t.toString());
+	t.remove();
+	out("After");
+	out(t.toString() + "\n");
+	out("Before Removal of: " + t.size());
+	out(t.toString());
+	t.remove();
+	out("After");
+	out(t.toString() + "\n");
+	out("Before Removal of: " + t.size());
+	out(t.toString());
+	t.remove();
+	out("After");
+	out(t.toString() + "\n");
+	out("Before Removal of: " + t.size());
+	out(t.toString());
+	t.remove();
+	out("After");
+	out(t.toString() + "\n");
+    }
 }
--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -2,116 +2,97 @@ import java.io.*;
 import java.util.*;
 
 public class MyHeap{
-  private int[]data;
-  private boolean isMax;
-  private int current;
+    private int[]data;
+    private boolean isMax;
+    private int current;
 
-  public MyHeap(){
-    data = new int[50];
-    data[0]=0;
-    current=1;
-    isMax = true;
-  }
+    public MyHeap(){
+	data = new int[50];
+	data[0]=0;
+	current=1;
+	isMax = true;
+    }
   
-  //true = max heap
-  //false = min heap
-  public MyHeap(boolean x){
-    data = new int[100];
-    data[0]=0;
-    current = 1;
-    isMax = x;
-  }
+    //true = max heap
+    //false = min heap
+    public MyHeap(boolean x){
+	data = new int[100];
+	data[0]=0;
+	current = 1;
+	isMax = x;
+    }
 
-  public String size(){
-    return "" + data[0];
-  }
+    public String size(){
+	return "" + data[0];
+    }
 
-  public void add(int value){
-    //      System.out.println("Adding " + value);
-    data[0]=data[0]+1;
-    if(current==1){
+    public void add(int value){
+	//      System.out.println("Adding " + value);
+	data[0]=data[0]+1;
+	if(current==1){
 	    data[1]=value;
-    }else{
+	}else{
 	    data[current]=value;
 	    if(value > data[current/2]){
-        int m = current;
-        int i = current/2;
-        int temp;
-        while(i !=0 && value > data[i]){
-          temp = data[i];
-          data[i]=value;
-          data[m]=temp;
-          m=i;
-          i=i/2;
-        }
+		int m = current;
+		int i = current/2;
+		int temp;
+		while(i !=0 && value > data[i]){
+		    temp = data[i];
+		    data[i]=value;
+		    data[m]=temp;
+		    m=i;
+		    i=i/2;
+		}
 	    }
+	}
+	current++;
+	//    System.out.println(toString());
     }
-    current++;
-    //    System.out.println(toString());
-  }
 
-  public int peek(){
-    return data[1];
-  }
-
-  public int remove(){
-    if(data[0]==0){
-      throw new NoSuchElementException();
-    }else{
-    data[0]=data[0]-1;
-    int out = data[1];
-    data[1] = data[current-1];
-    current--;
-    System.out.println("Current: " +  current);
-    int i=1;
-    int temp;
-    System.out.println(toString());
-    while(i<current){
-      temp=data[i];
-      if(data[i*2]>data[i*2+1]){
-        data[i]=data[i*2];
-        data[i*2]=temp;
-        i=i*2;
-        System.out.println(i);
-      }else{
-        data[i]=data[i*2+1];
-        data[i*2+1]=temp;
-        i=i*2+1;
-        System.out.println(i);
-      }
-      System.out.println("in loop: " + toString());
+    public int peek(){
+	return data[1];
     }
-    // data[0]=data[0]-1;
-    return out;
-    }
-  }
-    /*
-    int i=1;
-    int temp;
-    while(i<current){
-      temp = data[i];
-      if(data[i*2]>data[i*2+1]){
-        data[i]=data[i*2];
-        data[i*2]=temp;
-        i=i*2;
-      }else{
-        data[i]=data[i*2+1];
-        data[i*2+1]=temp;
-        i=i*2+1;
-      }
-    }
-    data[0]=data[0]-1;
-    current--;
-    
-    return out;
-    }*/
 
+    public int remove(){
+	if(data[0]==0){
+	    throw new NoSuchElementException();
+	}else{
+	    data[0]=data[0]-1;
+	    int out = data[1];
+	    data[1] = data[current-1];
+	    current--;
+	    //    System.out.println("Current: " +  current);
+	    int i=1;
+	    int temp;
+	    //	    System.out.println(toString());
+	    while(i<current && i*2<current && i*2+1<current &&
+		  (i<data[i*2] || i<data[i*2+1])){
+		//	System.out.println("Current: " + current);
+		temp=data[i];
+		if(data[i*2]>data[i*2+1]){
+		    data[i]=data[i*2];
+		    data[i*2]=temp;
+		    i=i*2;
+		    //	    System.out.println(i);
+		}else{
+		    data[i]=data[i*2+1];
+		    data[i*2+1]=temp;
+		    i=i*2+1;
+		    //	    System.out.println(i);
+		}
+		//	System.out.println("in loop: " + toString());
+	    }
+	    // data[0]=data[0]-1;
+	    return out;
+	}
+    }
 
-  public String toString(){
-    String out = "[ ";
-    for(int i=1;i<=data[0];i++){
+    public String toString(){
+	String out = "[ ";
+	for(int i=1;i<=data[0];i++){
 	    out = out + data[i] + " ";
+	}
+	return out +"]";
     }
-    return out +"]";
-  }
 }
